aab5d5cba7e9e46a5bc52f18934aa178435b7753
Lucy <lucywang340@gmail.com>
2025-07-14 15:01:24 -0500

    made the font bigger and added an image

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -9,40 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing:10){
+        VStack(spacing:1){
             Text("Welcome")
-                .padding()
-                .font(.title)
+                .font(.system(size:80))
                 .fontWeight(.heavy)
                 
                 .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
                 .multilineTextAlignment(.center)
-            Text("KLZR Glow")
-                .font(.title2)
+            Text("to KLZR Glow")
+                .font(.system(size:40))
             
                 .padding()
              
                 Text("Our story:")
-                  
+                .font(.system(size:25))
                 Text("We wanted to create an app that have unbiased advice about skincare. ")
+                .font(.system(size:20))
                 Text("")
                  
                 
               
                     Text("Purpose:")
+                    .font(.system(size:25))
+
                     Text("Based on your skin type a  series of skncare prodcuts will be recommended.")
+                .font(.system(size:20))
+
                     Text("")
                     Text("")
                     HStack{
                         
                         Text("Click to learn your skin type")
+                            .font(.system(size:20))
+
                         Button("Click me") {
                             /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
                         
                         }
-                       
+                        .font(.system(size:20))
+
+
                     }
-                
+            Image("Image")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
             
         }
         Spacer()
