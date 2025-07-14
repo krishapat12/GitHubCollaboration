f638b36722539957b7ddb4dfaad413d3bc6cc651
Lucy <lucywang340@gmail.com>
2025-07-14 13:27:57 -0500

    added edits

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -11,22 +11,41 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing:10){
             Text("Welcome")
-                .font(.largeTitle)
+                .padding()
+                .font(.title)
                 .fontWeight(.heavy)
+                
                 .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
                 .multilineTextAlignment(.center)
             Text("KLZR Glow")
+                .font(.title2)
             
-                VStack{
-                    Text("Our story:")
-                    Text("We wanted to create an app that have unbiased advice about skincare. ")
-                }
-                VStack{
+                .padding()
+             
+                Text("Our story:")
+                  
+                Text("We wanted to create an app that have unbiased advice about skincare. ")
+                Text("")
+                 
+                
+              
                     Text("Purpose:")
                     Text("Based on your skin type a  series of skncare prodcuts will be recommended.")
-                }
+                    Text("")
+                    Text("")
+                    HStack{
+                        
+                        Text("Click to learn your skin type")
+                        Button("Click me") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        
+                        }
+                       
+                    }
+                
             
         }
+        Spacer()
     }
 }
 #Preview {
