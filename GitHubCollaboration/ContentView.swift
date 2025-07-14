f9847fadf0e356bf0c015b75bbbb4a4514dca16f
Lucy <lucywang340@gmail.com>
2025-07-14 13:14:49 -0500

    welcome, purpose, mission

--- GitHubCollaboration/ContentView.swift
+++ GitHubCollaboration/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Change 1")
+        VStack(spacing:10){
+            Text("Welcome")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
+                .multilineTextAlignment(.center)
+            Text("KLZR Glow")
+            
+                VStack{
+                    Text("Our story:")
+                    Text("We wanted to create an app that have unbiased advice about skincare. ")
+                }
+                VStack{
+                    Text("Purpose:")
+                    Text("Based on your skin type a  series of skncare prodcuts will be recommended.")
+                }
+            
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
