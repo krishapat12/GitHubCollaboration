40705976c76317ea15fcc718942a3d09e1da9e88
Lucy <lucywang340@gmail.com>
2025-07-15 09:58:09 -0500

    view 1 Lucy

--- DrySkinView.swift
+++ DrySkinView.swift
@@ -13,13 +13,23 @@ struct DrySkinView: View {
             Text("Dry Skin")
                 .font(.system(size:80))
                 .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
+            Text("Learn about cleanser, toner, serum, and mosturizer from the dropdown below. We have included a variety of products to give options. ")
+                .multilineTextAlignment(.center)
+            List{
+                Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Pick a product")) {
+                    /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
+                    /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
+                }
+
+            }
            
             }
             Spacer()
         }
     }
-}
+
 
 #Preview {
     DrySkinView()
 }
+ 
