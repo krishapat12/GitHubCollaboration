c6897eee5e366947e428baf890943894ab8c57f1
Lucy <lucywang340@gmail.com>
2025-07-15 10:52:41 -0500

    dry skin page

--- DrySkinView.swift
+++ DrySkinView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct DrySkinView: View {
+    @State var choiceMade = "Options"
     var body: some View {
         VStack{
             Text("Dry Skin")
@@ -15,19 +16,52 @@ struct DrySkinView: View {
                 .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
             Text("Learn about cleanser, toner, serum, and mosturizer from the dropdown below. We have included a variety of products to give options. ")
                 .multilineTextAlignment(.center)
-            List{
-                Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Pick a product")) {
-                    /*@START_MENU_TOKEN@*/Text("1").tag(1)/*@END_MENU_TOKEN@*/
-                    /*@START_MENU_TOKEN@*/Text("2").tag(2)/*@END_MENU_TOKEN@*/
-                }
+            
+            Text("")
+            Text("Dropdown menu")
+            Menu{
+                Button(action:{
+                    choiceMade="Cleanser"
+                    //add the code to that pops up the description of the cleanser
+                },label:{
+                    Text("Cleanser")
+                        .foregroundColor(Color.black)
+                })
+                Button(action:{
+                    choiceMade="Serum"
+                    //add the code to that pops up the description of the serum
 
+                },label:{
+                    Text("Serum")
+                })
+                Button(action:{
+                    choiceMade="Mosturizer"
+                    //add the code to that pops up the description of the mosturizer
+
+                },label:{
+                    Text("Mosturizer")
+                })
+                Button(action:{
+                    choiceMade="Toner"
+                    //add the code to that pops up the description of the toner
+
+                },label:{
+                    Text("Toner")
+                })
             }
-           
+            label:{
+                Label(
+                    title: {Text("\(choiceMade)") },
+                    icon:{Image(systemName:"plus")}
+                )
+            
             }
+            .frame(width: 300.0, height: 50.0)
+            .border(Color.black, width:2)
             Spacer()
         }
     }
-
+}
 
 #Preview {
     DrySkinView()
